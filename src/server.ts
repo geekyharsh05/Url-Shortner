9ed2c81bd0466251d8e5a6fc7a74cd1b772491e6
geekyharsh05 <hp424805@gmail.com>
2023-11-21 23:21:24 +0530

    feat: add swagger for API documentation

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { dbConnect } from './db/db.connect';
 import route from './routes/url.route';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './utils/swagger.json'
 
 dotenv.config();
 dbConnect();
@@ -13,6 +15,7 @@ const PORT = process.env.PORT ?? 4001;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', route);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT: http://localhost:${PORT}`);
